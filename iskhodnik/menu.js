22da3bac8ce16314d911a0d16b8f649d4f0ad984
Zhenya <12A92@mail.ru>
2022-06-18 00:44:41 +0500

    Сборка Вебпак полностью кроме папки Node Modules

---
+++ iskhodnik/menu.js
@@ -0,0 +1,40 @@
+let knopka_menu;
+let samo_menu;
+let ikonka_menu;
+let krestik_menu;
+
+knopka_menu = document.getElementById('knopka_menu');
+samo_menu = document.getElementById('menu');
+ikonka_menu = document.getElementById('ikonka');
+krestik_menu = document.getElementById('krestik');
+
+function otkryit_zakryit () {
+    ikonka_menu.classList.toggle('ne_vidna_iconka_menu');
+    krestik_menu.classList.toggle('ne_viden_container_zakryit_menu');
+    samo_menu.classList.toggle('ne_vidno_menu');
+}
+
+knopka_menu.addEventListener('click', otkryit_zakryit);
+
+import html from './index.html';
+import htm from './kontaktyi.html';
+import ht from './tovaryi.html';
+import h from './uslugi.html';
+
+import gl from './images/gl.png';
+import head from './images/head.png';
+import ikonka3 from './images/ikonka3.png';
+import ikonkaf from './images/ikonkafejsbuka.png';
+import ikonkai from './images/ikonkainternet.png';
+import ikonkat from './images/ikonkatwittera.png';
+import menu from './images/menu.png';
+import play from './images/play.png';
+import red from './images/red.png';
+import sec from './images/sec.png';
+import sec2 from './images/sec2.png';
+import sec3 from './images/sec3.png';
+import sec4 from './images/sec4.png';
+import sky from './images/sky.png';
+import vector from './images/vector.png';
+
+import less from './lessDir/container.less';
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './iskhodnik/menu.js',
+    
+    output: {
+        path: path.resolve(__dirname, 'gotovo'),
+        filename: 'menu.js'
+  },
+    
+  plugins: [
+       
+       new HtmlWebpackPlugin({
+           template: './iskhodnik/index.html'
+       }),
+       new HtmlWebpackPlugin({
+           filename: 'kontaktyi.html',
+           template: './iskhodnik/kontaktyi.html'
+       }),
+      new HtmlWebpackPlugin({
+          filename: 'tovaryi.html',
+          template: './iskhodnik/tovaryi.html'
+      }),
+      new HtmlWebpackPlugin({
+          filename: 'uslugi.html',
+          template: './iskhodnik/uslugi.html'
+      }),
+       new MiniCssExtractPlugin()
+   ],
+    module: {
+        rules: [
+            {
+                test: /\.(ttf)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {esModule: false, name: './[name].[ext]', outputPath: 'shr'}
+                }],
+                type: 'javascript/auto'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                          loader: 'file-loader',
+                          options: {esModule: false, name: './[name].[ext]', outputPath: 'imag'}
+                     }],
+                type: 'javascript/auto'
+            },
+            {
+                test: /\.less$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+            },
+            {
+               test: /\.html$/i,
+                loader: 'html-loader'
+            }
+        ]
+    }
+}
