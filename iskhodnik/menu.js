eebe9265bba13010300601905882cfb3735f6a0a
Zhenya <12A92@mail.ru>
2022-07-24 23:09:13 +0500

    header #16

--- iskhodnik/menu.js
+++ iskhodnik/menu.js
@@ -1,20 +1,14 @@
-let knopka_menu;
-let samo_menu;
-let ikonka_menu;
-let krestik_menu;
+let buttonMenu;
+let navMenu;
 
-knopka_menu = document.getElementById('knopka_menu');
-samo_menu = document.getElementById('menu');
-ikonka_menu = document.getElementById('ikonka');
-krestik_menu = document.getElementById('krestik');
+buttonMenu = document.getElementById('btn');
+navMenu = document.getElementById('menu');
 
-function otkryit_zakryit () {
-    ikonka_menu.classList.toggle('ne_vidna_iconka_menu');
-    krestik_menu.classList.toggle('ne_viden_container_zakryit_menu');
-    samo_menu.classList.toggle('ne_vidno_menu');
+function openClose () {
+    navMenu.classList.toggle('header__navigation_visible');
 }
 
-knopka_menu.addEventListener('click', otkryit_zakryit);
+buttonMenu.addEventListener('click', openClose);
 
 import html from './htmlFiles/index.html';
 import htm from './htmlFiles/kontaktyi.html';
