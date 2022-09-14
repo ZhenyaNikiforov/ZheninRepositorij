5abf260c4197fe7146fc3ba2cb3ffc45818173eb
Zhenya <12A92@mail.ru>
2022-09-14 21:00:20 +0500

    forEach((e)=>new object(e, y, z)); document.querySelectorAll('.classOfElements'); block__element_modifier; block__element; //--card.pug #31

--- src/menu.js
+++ src/menu.js
@@ -3,23 +3,22 @@ class Gamburger {
         this.button= btn;
         this.table= menu;
         this.opClass= open;
-        this.button.addEventListener('click', this.OpCl.bind(null, this.table, this.opClass));
+        this.button.addEventListener('click', this.openClose.bind(null, this.table, this.opClass));
     }
-    OpCl(a, b){
+    openClose(a, b){
         a.classList.toggle(b);
     }
 }
 
-const elementMenu= document.getElementsByClassName('header__navigation')[0];
+const elementMenu= document.querySelectorAll('.header__navigation')[0];
 const openClass= 'header__navigation_visible';
 
-function gumbs(nameObject){
-    const objectButtons= document.getElementsByClassName(nameObject);
-    const arrayButtons= Array.from(objectButtons);
-    arrayButtons.forEach(function(elemArray){new Gamburger(elemArray, elementMenu, openClass)});
+function render(nameSelectors){
+    const arrayButtons= document.querySelectorAll(nameSelectors);
+    arrayButtons.forEach((elemArray)=> new Gamburger(elemArray, elementMenu, openClass));
 }
 
-gumbs('header__btn-menu');
+render('.header__btn-menu');
 
 import html from './pages/index.pug';
 import htm from './pages/kontaktyi.pug';
